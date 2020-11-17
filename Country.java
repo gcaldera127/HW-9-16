3983021e5c9cb1f32dddc9e23405c91044a43531
gcaldera127 <60445013+gcaldera127@users.noreply.github.com>
2020-11-16 22:24:48 -0500

    Add files via upload

---
+++ Country.java
@@ -0,0 +1,35 @@
+public class Country {
+    public static void main(String[] args) {
+        
+        private String name;
+        private String currency;
+        private boolean hasPyramids;
+        private String officialLanguage;
+
+        public Country{
+            public String getName (){
+             return "Canada";   
+            }
+            public String getCurrency (){
+                return "Canadian Dollar";
+            }
+            public boolean hasPyramids(){
+                return false;
+            }    
+        }
+        public Country{
+            public String setName (){
+                return "Egypt";
+            }
+            public String setCurrency(){
+                return "Egyptian Pound";
+            }
+            public String setOfficialLanguage(){
+                return "Arabic";
+            }
+        }
+
+
+    }
+        
+}
---
+++ Pet.java
@@ -0,0 +1,39 @@
+public class Pet {
+    public static void main(String[] args){
+
+        private String name;
+        private int age;
+        private String location;
+        private String type;
+
+        public Pet() {
+            public String getName(){
+                return "Daisy";
+            }
+            public int getAge(){
+                return 1;
+            }
+            public String getType(){
+                return "Golden Retriever";
+            }
+        }
+
+        public Pet() {
+            public String getName(){
+                return "Buddy";
+            }
+            public int getAge(){
+                return 14;
+            }
+            public String getLocation(){
+                return "Kentucky";
+            }
+
+        }
+
+
+
+            
+    }
+}
+
